b1b1c109a61b5f228def3c507a40329f09b14049
Christian Mendez <christian.mndz@outlook.com>
2023-01-21 21:14:49 -0600

    first commit

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=250)
+    birthday = models.DateField()
